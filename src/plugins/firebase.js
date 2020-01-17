55a04c3f69123dd2e813127218b32c822338fe38
sultanmyrza <sultanmyrza@gmail.com>
2020-01-17 11:28:12 +0800

    add anonymous signin

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -37,6 +37,19 @@ const firestore = firebase.firestore()
 const auth = firebase.auth()
 const currentUser = auth.currentUser
 
+export async function getAnonymousUser() {
+  if (auth.currentUser) {
+    return auth.currentUser
+  }
+
+  try {
+    const userCred = await auth.signInAnonymously()
+    return userCred.user
+  } catch (error) {
+    return null
+  }
+}
+
 // firebase collections
 const customersCollection = firestore.collection('customers')
 
